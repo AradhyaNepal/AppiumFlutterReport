967b2a8e2bf4170da2fc6b1af97ac9f947aaf7fc
Aradhya Nepal <aradhya.1221@gmail.com>
2023-04-25 17:18:34 +0545

    Drag and Drop

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -15,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool _isHovering = false;
+  DropzoneViewController? drapDropController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,104 +28,109 @@ class _HomeScreenState extends State<HomeScreen> {
             DropzoneView(
               operation: DragOperation.copy,
               cursor: CursorType.grab,
+              onCreated: (DropzoneViewController ctrl) =>
+                  drapDropController = ctrl,
               onHover: () {
-                if (_isHovering == false)
-                  setState(() {
-                    _isHovering = true;
-                  });
+                _updateHovering(true);
               },
               onDrop: (dynamic ev) {
-                setState(() {
-                  _isHovering = false;
-                });
+                _updateHovering(false);
+                getDataFromDrag(ev);
               },
               onLeave: () {
-                setState(() {
-                  _isHovering = false;
-                });
+                _updateHovering(false);
               },
             ),
-            Container(
-              height: 500.h,
-              width: 500.h,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: Theme.of(context).primaryColor, width: 10),
-                borderRadius: BorderRadius.circular(10.r),
-              ),
-              padding: EdgeInsets.symmetric(
-                horizontal: 20.w,
-                vertical: 20.h,
-              ),
-              margin: EdgeInsets.symmetric(
-                vertical: 20.h,
-                horizontal: 20.w,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.fitHeight,
-                      child: Icon(
-                        Icons.image_outlined,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 500.h,
+                  width: 500.h,
+                  decoration: BoxDecoration(
+                    color: _isHovering
+                        ? Theme.of(context).primaryColor.withOpacity(0.2)
+                        : null,
+                    border: Border.all(
+                        color: Theme.of(context).primaryColor, width: 10),
+                    borderRadius: BorderRadius.circular(10.r),
                   ),
-                  SizedBox(
-                    height: 20.h,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.w,
+                    vertical: 20.h,
                   ),
-                  InkWell(
-                    onTap: () async {
-                      FilePickerResult? result =
-                          await FilePicker.platform.pickFiles();
-                      if (result == null) return;
-                      final value = result.files.first.bytes;
-                      if (value == null) return;
-                      String jsonContent = String.fromCharCodes(value);
-                      final response = json.decode(jsonContent);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5.h,
-                        horizontal: 10.w,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).primaryColor, width: 3),
-                        borderRadius: BorderRadius.circular(5.r),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "Import",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Icon(
-                            Icons.upload,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 20.h,
+                    horizontal: 20.w,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.fitHeight,
+                          child: Icon(
+                            Icons.image_outlined,
                             color: Theme.of(context).primaryColor,
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          FilePickerResult? result =
+                              await FilePicker.platform.pickFiles();
+                          if (result == null) return;
+                          final value = result.files.first.bytes;
+                          if (value == null) return;
+                          String jsonContent = String.fromCharCodes(value);
+                          final response = json.decode(jsonContent);
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 5.h,
+                            horizontal: 10.w,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).primaryColor,
+                                width: 3),
+                            borderRadius: BorderRadius.circular(5.r),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "Import",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Icon(
+                                Icons.upload,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Text(
+                        "Or\nDrag And Drop",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Text(
-                    "Or\nDrag And Drop",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
@@ -133,5 +139,17 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void updateHovering() {}
+  void getDataFromDrag(dynamic htmlData) async {
+    if (drapDropController == null) return;
+    final value = await drapDropController!.getFileData(htmlData);
+    String jsonContent = String.fromCharCodes(value);
+    final response = json.decode(jsonContent);
+  }
+
+  void _updateHovering(bool value) {
+    if (_isHovering == value) return;
+    setState(() {
+      _isHovering = value;
+    });
+  }
 }
