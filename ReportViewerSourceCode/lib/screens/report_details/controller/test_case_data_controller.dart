d655799936b7923c45f9f1678899d1d1194426b0
Aradhya Nepal <aradhya.1221@gmail.com>
2023-04-28 12:48:57 +0545

    Refractoring TestCaseDataController Code

--- ReportViewerSourceCode/lib/screens/report_details/controller/test_case_data_controller.dart
+++ ReportViewerSourceCode/lib/screens/report_details/controller/test_case_data_controller.dart
@@ -1,4 +1,5 @@
 import 'package:appium_report/model/report.dart';
+import 'package:appium_report/model/test_case.dart';
 import 'package:appium_report/screens/report_details/model/test_case_row_data.dart';
 
 class TestCaseDataController {
@@ -18,36 +19,57 @@ class TestCaseDataController {
   }
 
   void expandChildren(TestCaseRowData testCaseRowData) {
-    int parentIndex = testCaseRowData.currentIndex;
-    List<TestCaseRowData> expandedData = [];
     if (testCaseRowData.testCase.children == null) return;
+    List<TestCaseRowData> expandedData = _getExpandedChildList(testCaseRowData);
+    _updateUIListWithNewExpandedData(testCaseRowData, expandedData);
+  }
+
+  List<TestCaseRowData> _getExpandedChildList(TestCaseRowData testCaseRowData) {
+    List<TestCaseRowData> expandedData = [];
     final children = testCaseRowData.testCase.children ?? [];
+    int parentIndex = testCaseRowData.currentIndex;
     for (int i = 0; i < children.length; i++) {
-      ChildType childType = ChildType.mid;
-      if (i == 0) {
-        childType = ChildType.first;
-      } else if (i == children.length - 1) {
-        childType = ChildType.last;
-      }
+      ChildType childType = _getChildType(i, children);
       expandedData.add(
         TestCaseRowData(
           testCase: children[i],
-          childOpenedData: ChildOpenedData(
-            childType: childType,
-            parents: [
-              ...testCaseRowData.childOpenedData?.parents ?? [],
-              testCaseRowData.testCase.testName
-            ],
-            parentIndexLocation: [
-              ...testCaseRowData.childOpenedData?.parentIndexLocation ?? [],
-              parentIndex
-            ],
-          ),
+          childOpenedData:
+              _getChildOpenedData(childType, testCaseRowData, parentIndex),
           currentIndex: parentIndex + i,
           isGroup: children[i].children != null,
         ),
       );
     }
+    return expandedData;
+  }
+
+  ChildOpenedData _getChildOpenedData(
+      ChildType childType, TestCaseRowData testCaseRowData, int parentIndex) {
+    return ChildOpenedData(
+      childType: childType,
+      parents: [
+        ...testCaseRowData.childOpenedData?.parents ?? [],
+        testCaseRowData.testCase.testName
+      ],
+      parentIndexLocation: [
+        ...testCaseRowData.childOpenedData?.parentIndexLocation ?? [],
+        parentIndex
+      ],
+    );
+  }
+
+  ChildType _getChildType(int i, List<TestCase> children) {
+    ChildType childType = ChildType.mid;
+    if (i == 0) {
+      childType = ChildType.first;
+    } else if (i == children.length - 1) {
+      childType = ChildType.last;
+    }
+    return childType;
+  }
+
+  void _updateUIListWithNewExpandedData(
+      TestCaseRowData testCaseRowData, List<TestCaseRowData> expandedData) {
     bool isNotFirst = testCaseRowData.currentIndex != 0;
     bool isNotLast =
         testCaseRowData.currentIndex != testCaseWidgetList.length - 1;
